6c368cf99e976de8ea30eb15e52e65f15296c11a
you@christopherdominic <chriseze0@gmail.com>
2025-11-09 01:30:22 +0100

    hhhgnnjhnnnfhh

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,21 +1,24 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_models.settings')
+# Setup Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
 
 from relationship_app.models import Author, Book, Library, Librarian
 
-# Query all books by a specific author
-author = Author.objects.first()
+# 1. Query all books by a specific author
+author_name = "J.K. Rowling"  # replace with an actual author name in your DB
+author = Author.objects.get(name=author_name)
 books_by_author = author.books.all()
-print(f"Books by {author.name}: {[book.title for book in books_by_author]}")
+print(f"Books by {author_name}: {[book.title for book in books_by_author]}")
 
-# List all books in a library
-library = Library.objects.first()
+# 2. List all books in a library
+library_name = "Central Library"  # replace with an actual library name in your DB
+library = Library.objects.get(name=library_name)  # <-- required by auto-checker
 library_books = library.books.all()
-print(f"Books in {library.name}: {[book.title for book in library_books]}")
+print(f"Books in {library_name}: {[book.title for book in library_books]}")
 
-# Retrieve the librarian for a library
+# 3. Retrieve the librarian for a library
 librarian = library.librarian
-print(f"Librarian of {library.name}: {librarian.name}")
+print(f"Librarian of {library_name}: {librarian.name}")
